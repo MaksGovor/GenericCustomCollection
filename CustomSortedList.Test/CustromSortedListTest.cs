bceecb2aaf97af9bb83b758fdd2663d0207b8bd3
MaksGovor <maksgovruha@gmail.com>
2022-04-10 22:22:49 +0300

    add first test

--- CustomSortedList.Test/CustromSortedListTest.cs
+++ CustomSortedList.Test/CustromSortedListTest.cs
@@ -1,4 +1,5 @@
 using System;
+using CustomSortedList;
 using Xunit;
 
 namespace CustomSortedList.Test
@@ -6,9 +7,19 @@ namespace CustomSortedList.Test
     public class CustromSortedListTest
     {
         [Fact]
-        public void Test1()
+        public void AddingKeysAndValueMatching()
         {
+            MySortedList<string, int> nubmers = new MySortedList<string, int>();
+            string[] keys = new string[] { "one", "two", "three" };
+            int[] values = new int[] { 1, 2, 3};
 
+            for (int i = 0; i < keys.Length; i++)
+            {
+                string key = keys[i];
+                int value = values[i];
+                nubmers.Add(key, value);
+                Assert.Equal(nubmers[key], value);
+            }
         }
     }
 }
