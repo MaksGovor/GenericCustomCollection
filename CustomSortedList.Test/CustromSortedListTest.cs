9937d78aabefd2585ce350ac22636af1c9d6ef73
MaksGovor <maksgovruha@gmail.com>
2022-04-10 21:28:35 +0300

    add xUnit test project

---
+++ CustomSortedList.Test/CustromSortedListTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomSortedList.Test
+{
+    public class CustromSortedListTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
